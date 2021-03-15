4f5577b044c843d76c953b575aa5e44ad67a68e2
ir-afn <thunderburker@gmail.com>
2021-03-15 23:01:25 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let open;
+  let close;
+  for (const item of bracketsConfig) {
+    open = item[0];
+    close = item[1];
+    for (let i = 0; i < str.length; i += 1) {
+      let symbol = str[i];
+      if (symbol === open) {
+        stack.push(symbol);
+      } else if (symbol === close) {
+        if (!stack.pop()) { 
+          return false;
+        }
+      }
+    }
+  }
+  return stack.length === 0;
 }
